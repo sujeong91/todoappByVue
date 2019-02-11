127d45c28840c0d412fa88a162217ca313d1f056
sujeong91 <susu.91@daum.net>
2019-02-11 13:27:25 +0900

    first commit

---
+++ src/components/Example.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">Panel heading without title</div>
+    <div class="panel-body">
+      Panel content
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Example",
+    data() {
+      return {
+        msg: 'Basic panel example'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
